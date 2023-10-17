a6291e9332b599db4232298570342d29ba338f58
Glory77 <glory77@mail.ru>
2023-10-17 20:14:47 +0200

    category create validation

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -7,6 +7,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 #[ApiResource]
@@ -17,6 +18,7 @@ class Category
     #[ORM\Column]
     private int $id;
 
+    #[Assert\NotBlank]
     #[ORM\Column(length: 255)]
     private string $title;
 
--- src/Service/CategoryService.php
+++ src/Service/CategoryService.php
@@ -3,13 +3,17 @@
 namespace App\Service;
 
 use App\Entity\Category;
-use App\Model\CategoryUpdateRequest;
 use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class CategoryService
 {
-    public function __construct(private CategoryRepository $categoryRepository)
+    public function __construct(
+        private CategoryRepository $categoryRepository,
+        private ValidatorInterface $validator
+    )
     {
     }
 
@@ -23,7 +27,7 @@ class CategoryService
         return $this->categoryRepository->find($id);
     }
 
-    public function createCategory(Request $request): Category
+    public function createCategory(Request $request): Category | JsonResponse
     {
         $category = new Category();
         return $this->modifyCategory($request, $category);
@@ -41,10 +45,18 @@ class CategoryService
         $this->categoryRepository->remove($category, true);
     }
 
-    private function modifyCategory(Request $request, Category $category): Category
+    private function modifyCategory(Request $request, Category $category): Category | JsonResponse
     {
         $data = json_decode($request->getContent(), true);;
         $category->setTitle($data['title']);
+
+        $errors = $this->validator->validate($category);
+        if ($errors->count() > 0) {
+            return new JsonResponse([
+                'message' => (string) $errors
+            ]);
+        }
+
         $this->categoryRepository->save($category, true);
         return $category;
     }
