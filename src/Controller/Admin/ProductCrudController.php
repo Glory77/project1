66e1650d16b8f3f3ab8fb8108c1fd15314aee7d2
Glory77 <glory77@mail.ru>
2023-10-17 11:16:12 +0200

    product category

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -12,14 +17,18 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            //IdField::new('id'),
+            AssociationField::new('category')
+                ->setFormTypeOption('by_reference', true)
+                ->hideOnIndex(),
             TextField::new('title'),
             TextEditorField::new('description'),
+            MoneyField::new('price')
+                ->setCurrency('EUR'),
+            BooleanField::new('active'),
         ];
     }
-    */
 }
